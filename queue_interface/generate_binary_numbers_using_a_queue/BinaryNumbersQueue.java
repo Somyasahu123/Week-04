acb46a8a20502422e7c21f537fa2ac3052bb3ecf
Somyasahu123 <somyasahu221@gmail.com>
2025-02-12 13:49:10 +0530

    SOMYA SAHU Reverse Queue

---
+++ queue_interface/generate_binary_numbers_using_a_queue/BinaryNumbersQueue.java
@@ -0,0 +1,27 @@
+package queue_interface.generate_binary_numbers_using_a_queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BinaryNumbersQueue {
+
+    public static void generateBinaryNumbers(int N) {
+        Queue<String> queue = new LinkedList<>();
+        queue.add("1"); // Start with "1"
+
+        for (int i = 0; i < N; i++) {
+            String binary = queue.poll(); // Remove the front element
+            System.out.print(binary + " "); // Print the binary number
+
+            queue.add(binary + "0"); // Enqueue next binary numbers
+            queue.add(binary + "1");
+        }
+    }
+
+    public static void main(String[] args) {
+        int N = 5;
+        System.out.println("First " + N + " binary numbers:");
+        generateBinaryNumbers(N);
+    }
+}
+
