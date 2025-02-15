9cb9c156cf3d95513d49144fdd7a54d513456d01
Somyasahu123 <somyasahu221@gmail.com>
2025-02-16 00:01:00 +0530

    [SOMYA SAHU] Add. User

---
+++ annotations/practiceproblems/advanced/roleallowed/User.java
@@ -0,0 +1,14 @@
+package annotations.practiceproblems.advanced.roleallowed;
+
+class User {
+    private String role;
+
+    public User(String role) {
+        this.role = role;
+    }
+
+    public String getRole() {
+        return role;
+    }
+}
+
